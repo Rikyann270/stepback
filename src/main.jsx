a03f92866583277476534af3d32340f657aa0c0a
Rikyann270 <rikyann270@gmail.com>
2024-01-12 06:46:13 -0800

    router failer test

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Chickspage from './products/Chickspage.jsx'
-import Datepage from './date/Datepage.jsx'
 // import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-    {
-    path: "chicks",
-    element: <Chickspage/>,
-  },
-  {
-    path: "date_confermation",
-    element: <Datepage/>,
-  },
-]);
-
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router} />
+    <App />
   </React.StrictMode>,
 )
